ed577c69a4ff01aac475da0818cde223971f4b3b
Steven Eisinger <steven.a.eisinger@gmail.com>
2023-05-11 00:35:26 +0200

    wip: more types added from the api

--- src/spacetraders/types.rs
+++ src/spacetraders/types.rs
@@ -1,6 +1,7 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::fmt;
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Agent {
     pub account_id: String,
@@ -9,7 +10,7 @@ pub struct Agent {
     pub credits: i32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Chart {
     pub waypoint_symbol: String,
@@ -17,42 +18,34 @@ pub struct Chart {
     pub submitted_on: String,
 }
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "UPPERCASE")]
-pub enum SystemType {
-    NeutronStar(String),
-    RedStar(String),
-    OrangeStar(String),
-    BlueStar(String),
-    YoungStar(String),
-    WhiteDwarf(String),
-    BlackHole(String),
-    Hypergiant(String),
-    Nebula(String),
-    Unstable(String),
-}
-
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ConnectedSystem {
     pub symbol: String,
     pub sector_symbol: String,
     pub r#type: SystemType,
+
+    #[serde(default)]
     pub faction_symbol: String,
     pub x: i32,
     pub y: i32,
     pub distance: i32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "UPPERCASE")]
 pub enum ContractType {
-    Procurement(String),
-    Transport(String),
-    Shuttle(String),
+    Procurement,
+    Transport,
+    Shuttle,
+}
+impl fmt::Display for ContractType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Contract {
     pub id: String,
@@ -64,7 +57,7 @@ pub struct Contract {
     pub expiration: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ContractDeliverGood {
     pub trade_symbol: String,
@@ -73,14 +66,14 @@ pub struct ContractDeliverGood {
     pub units_fulfilled: i32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ContractPayment {
     pub on_accepted: i32,
     pub on_fulfilled: i32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ContractTerms {
     pub deadline: String,
@@ -88,7 +81,7 @@ pub struct ContractTerms {
     pub deliver: Vec<ContractDeliverGood>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Cooldown {
     pub ship_symbol: String,
@@ -97,21 +90,21 @@ pub struct Cooldown {
     pub expiration: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Extraction {
     pub ship_symbol: String,
     pub r#yield: ExtractionYield,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ExtractionYield {
     symbol: String,
     units: i32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Faction {
     pub symbol: String,
@@ -121,71 +114,76 @@ pub struct Faction {
     pub traits: Vec<FactionTrait>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "UPPERCASE")]
 pub enum FactionTraitSymbol {
-    Bureaucratic(String),
-    Secretive(String),
-    Capitalistic(String),
-    Industrious(String),
-    Peaceful(String),
-    Distrustful(String),
-    Welcoming(String),
-    Smugglers(String),
-    Scavengers(String),
-    Rebellious(String),
-    Exiles(String),
-    Pirates(String),
-    Raiders(String),
-    Clan(String),
-    Guild(String),
-    Dominion(String),
-    Fringe(String),
-    Forsaken(String),
-    Isolated(String),
-    Localized(String),
-    Established(String),
-    Notable(String),
-    Dominant(String),
-    Inescapable(String),
-    Innovative(String),
-    Bold(String),
-    Visionary(String),
-    Curious(String),
-    Daring(String),
-    Exploratory(String),
-    Resourceful(String),
-    Flexible(String),
-    Cooperative(String),
-    United(String),
-    Strategic(String),
-    Intelligent(String),
-    ResearchFocused(String),
-    Collaborative(String),
-    Progressive(String),
-    Militaristic(String),
-    TechnologicallyAdvanced(String),
-    Aggressive(String),
-    Imperialistic(String),
-    TreasureHunters(String),
-    Dexterous(String),
-    Unpredictable(String),
-    Brutal(String),
-    Fleeting(String),
-    Adaptable(String),
-    SelfSufficient(String),
-    Defensive(String),
-    Proud(String),
-    Diverse(String),
-    Independent(String),
-    SelfInterested(String),
-    Fragmented(String),
-    Commercial(String),
-    FreeMarkets(String),
-    Entrepreneurial(String),
+    Bureaucratic,
+    Secretive,
+    Capitalistic,
+    Industrious,
+    Peaceful,
+    Distrustful,
+    Welcoming,
+    Smugglers,
+    Scavengers,
+    Rebellious,
+    Exiles,
+    Pirates,
+    Raiders,
+    Clan,
+    Guild,
+    Dominion,
+    Fringe,
+    Forsaken,
+    Isolated,
+    Localized,
+    Established,
+    Notable,
+    Dominant,
+    Inescapable,
+    Innovative,
+    Bold,
+    Visionary,
+    Curious,
+    Daring,
+    Exploratory,
+    Resourceful,
+    Flexible,
+    Cooperative,
+    United,
+    Strategic,
+    Intelligent,
+    ResearchFocused,
+    Collaborative,
+    Progressive,
+    Militaristic,
+    TechnologicallyAdvanced,
+    Aggressive,
+    Imperialistic,
+    TreasureHunters,
+    Dexterous,
+    Unpredictable,
+    Brutal,
+    Fleeting,
+    Adaptable,
+    SelfSufficient,
+    Defensive,
+    Proud,
+    Diverse,
+    Independent,
+    SelfInterested,
+    Fragmented,
+    Commercial,
+    FreeMarkets,
+    Entrepreneurial,
+}
+impl fmt::Display for FactionTraitSymbol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct FactionTrait {
     pub symbol: FactionTraitSymbol,
@@ -193,7 +191,7 @@ pub struct FactionTrait {
     pub description: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct JumpGate {
     pub jump_range: i32,
@@ -201,4 +199,480 @@ pub struct JumpGate {
     pub connected_systems: Vec<ConnectedSystem>,
 }
 
-// TODO: Add more types from https://spacetraders.stoplight.io/docs/spacetraders/11f2735b75b02-space-traders-api
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Market {
+    pub symbol: String,
+    pub exports: Vec<TradeGood>,
+    pub imports: Vec<TradeGood>,
+    pub exchange: Vec<TradeGood>,
+    pub transactions: Vec<MarketTransaction>,
+    pub trade_goods: Vec<MarketTradeGood>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct MarketTradeGood {
+    pub symbol: String,
+    pub trade_volume: i32,
+    pub supply: MarketTradeGoodSupply,
+    pub purchase_price: i32,
+    pub sell_price: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum MarketTradeGoodSupply {
+    Scarce,
+    Limited,
+    Moderate,
+    Abundant,
+}
+impl fmt::Display for MarketTradeGoodSupply {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct MarketTransaction {
+    pub waypoint_symbol: String,
+    pub ship_symbol: String,
+    pub trade_symbol: String,
+    pub r#type: String,
+    pub units: i32,
+    pub price_per_unit: i32,
+    pub total_price: i32,
+    pub timestanp: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Meta {
+    pub total: String,
+    pub page: String,
+    pub limit: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ScannedShip {
+    pub symbol: String,
+    pub registration: ShipRegistration,
+    pub nav: ShipNav,
+    pub frame: ShipFrame,
+    pub reactor: ShipReactor,
+    pub engine: ShipEngine,
+    pub mounts: Vec<ShipMount>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ScannedSystem {
+    pub symbol: String,
+    pub sector_symbol: String,
+    pub r#type: SystemType,
+    pub x: i32,
+    pub y: i32,
+    pub distance: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ScannedWaypoint {
+    pub symbol: String,
+    pub r#type: WaypointType,
+    pub system_symbol: String,
+    pub x: i32,
+    pub y: i32,
+    pub orbitals: Vec<WaypointOrbital>,
+    pub faction: WaypointFaction,
+    pub traits: Vec<WaypointTrait>,
+    pub chart: Vec<Chart>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Ship {
+    pub symbol: String,
+    pub registration: ShipRegistration,
+    pub nav: ShipNav,
+    pub crew: ShipCrew,
+    pub frame: ShipFrame,
+    pub reactor: ShipReactor,
+    pub engine: ShipEngine,
+    pub modules: Vec<ShipModule>,
+    pub mounts: Vec<ShipMount>,
+    pub cargo: ShipCargo,
+    pub fuel: ShipFuel,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipCargo {
+    pub capacity: i32,
+    pub units: i32,
+    pub inventory: Vec<ShipCargoItem>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipCargoItem {
+    pub symbol: String,
+    pub name: String,
+    pub description: String,
+    pub units: i32,
+}
+
+type ShipCondition = i32;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipCrew {
+    pub current: i32,
+    pub required: i32,
+    pub capacity: i32,
+
+    #[serde(default)]
+    pub rotation: ShipCrewRotation,
+    pub morale: ShipCrewMorale,
+    pub wages: i32,
+}
+
+type ShipCrewMorale = i32;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum ShipCrewRotation {
+    Strict,
+    Relaxed,
+}
+impl Default for ShipCrewRotation {
+    fn default() -> ShipCrewRotation {
+        ShipCrewRotation::Strict
+    }
+}
+impl fmt::Display for ShipCrewRotation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipEngine {
+    pub symbol: ShipEngineSymbol,
+    pub name: String,
+    pub description: String,
+    pub condition: ShipCondition,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum ShipEngineSymbol {
+    EngineImpulseDriveI,
+    EngineIonDriveI,
+    EngineIonDriveIi,
+    EngineHyperDriveI,
+}
+impl fmt::Display for ShipEngineSymbol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipFrame {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipFuel {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipModule {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipMount {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipNav {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipNavFlightMode {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipNavRoute {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipNavRouteWaypoint {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipNavStatus {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipReactor {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipRegistration {
+    pub name: String,
+    pub faction_symbol: String,
+    pub role: ShipRole,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipRequirements {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum ShipRole {
+    Fabricator,
+    Harvester,
+    Hauler,
+    Interceptor,
+    Excavator,
+    Transport,
+    Repair,
+    Surveyor,
+    Command,
+    Carrier,
+    Patrol,
+    Satellite,
+    Explorer,
+    Refinery,
+}
+impl fmt::Display for ShipRole {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipType {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Shipyard {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipyardShip {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ShipyardTransaction {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Survey {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SurveyDeposit {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct System {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SystemFaction {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum SystemType {
+    NeutronStar,
+    RedStar,
+    OrangeStar,
+    BlueStar,
+    YoungStar,
+    WhiteDwarf,
+    BlackHole,
+    Hypergiant,
+    Nebula,
+    Unstable,
+}
+impl fmt::Display for SystemType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SystemWaypoint {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct TradeGood {
+    pub symbol: TradeSymbol,
+    pub name: String,
+    pub description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum TradeSymbol {
+    PreciousStones,
+    QuartzSand,
+    SiliconCrystals,
+    AmmoniaIce,
+    LiquidHydrogen,
+    LiquidNitrogen,
+    IceWater,
+    ExoticMatter,
+    AdvancedCircuitry,
+    GravitonEmitters,
+    Iron,
+    IronOre,
+    Copper,
+    CopperOre,
+    Aluminum,
+    AluminumOre,
+    Silver,
+    SilverOre,
+    Gold,
+    GoldOre,
+    Platinum,
+    PlatinumOre,
+    Diamonds,
+    Uranite,
+    UraniteOre,
+    Meritium,
+    MeritiumOre,
+    Hydrocarbon,
+    Antimatter,
+    Fertilizers,
+    Fabrics,
+    Food,
+    Jewelry,
+    Machinery,
+    Firearms,
+    AssaultRifles,
+    MilitaryEquipment,
+    Explosives,
+    LabInstruments,
+    Ammunition,
+    Electronics,
+    ShipPlating,
+    Equipment,
+    Fuel,
+    Medicine,
+    Drugs,
+    Clothing,
+    Microprocessors,
+    Plastics,
+    Polynucleotides,
+    Biocomposites,
+    Nanobots,
+    AiMainframes,
+    QuantumDrives,
+    RoboticDrones,
+    CyberImplants,
+    GeneTherapeutics,
+    NeuralChips,
+    MoodRegulators,
+    ViralAgents,
+    MicroFusionGenerators,
+    Supergrains,
+    LaserRifles,
+    Holographics,
+    ShipSalvage,
+    RelicTech,
+    NovelLifeforms,
+    BotanicalSpecimens,
+    CulturalArtifacts,
+    ReactorSolarI,
+    ReactorFusionI,
+    ReactorFissionI,
+    ReactorChemicalI,
+    ReactorAntimatterI,
+    EngineImpulseDriveI,
+    EngineIonDriveI,
+    EngineIonDriveIi,
+    EngineHyperDriveI,
+    ModuleMineralProcessorI,
+    ModuleCargoHoldI,
+    ModuleCrewQuartersI,
+    ModuleEnvoyQuartersI,
+    ModulePassengerCabinI,
+    ModuleMicroRefineryI,
+    ModuleOreRefineryI,
+    ModuleFuelRefineryI,
+    ModuleScienceLabI,
+    ModuleJumpDriveI,
+    ModuleJumpDriveIi,
+    ModuleJumpDriveIii,
+    ModuleWarpDriveI,
+    ModuleWarpDriveIi,
+    ModuleWarpDriveIii,
+    ModuleShieldGeneratorI,
+    ModuleShieldGeneratorIi,
+    MountGasSiphonI,
+    MountGasSiphonIi,
+    MountGasSiphonIii,
+    MountSurveyorI,
+    MountSurveyorIi,
+    MountSurveyorIii,
+    MountSensorArrayI,
+    MountSensorArrayIi,
+    MountSensorArrayIii,
+    MountMiningLaserI,
+    MountMiningLaserIi,
+    MountMiningLaserIii,
+    MountLaserCannonI,
+    MountMissileLauncherI,
+    MountTurretI,
+}
+impl fmt::Display for TradeSymbol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Waypoint {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct WaypointFaction {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct WaypointOrbital {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct WaypointTrait {}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum WaypointType {
+    Planet,
+    GasGiant,
+    Moon,
+    OrbitalStation,
+    JumpGate,
+    AsteroidField,
+    Nebula,
+    DebrisField,
+    GravityWell,
+}
+impl fmt::Display for WaypointType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
