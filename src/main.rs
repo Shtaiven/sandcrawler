379c7dbc2b145d27edef3783f5e0e0c979518289
Steven Eisinger <steven.a.eisinger@gmail.com>
2023-05-14 19:46:28 +0200

    feat: take --register cli arg and prototype registration

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,43 @@
 pub mod spacetraders;
 
 use reqwest::Error;
+use spacetraders::api;
+use spacetraders::types;
 use std::env;
 use std::fs;
+
+fn print_help() {
+    println!("This is a help message")
+}
+
+async fn start_registration() -> Result<(), Error> {
+    println!("Registration started");
+    Ok(())
+}
+
+async fn start_cli(token_file: &str) -> Result<(), Error> {
+    println!("Starting cli with token file: {token_file}");
+    let token = fs::read_to_string(token_file).expect("Couldn't read token file contents");
+
+    // TODO: Turn this into a cli app
+    let agent = api::my_agent_details(token).await?;
+    println!("{:?}", agent);
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
 
-    // TODO: Handle arguments nicely
-    let token_file = &args[1];
-    println!("token file: {token_file}");
-    let token = fs::read_to_string(token_file)
-        .expect("Couldn't read token file contents");
-
-    // TODO: Turn this into a cli app
-    let agent = spacetraders::api::get_agent(token).await?;
-    println!("{:?}", agent);
+    println!("{:?}", args);
+    let _ = if args.len() > 2 {
+        match args[1].as_str() {
+            "--register" => start_registration().await?,
+            "--token" => start_cli(args[2].as_str()).await?,
+            _ => print_help(),
+        }
+    } else {
+        print_help()
+    };
     Ok(())
 }
--- src/spacetraders/api.rs
+++ src/spacetraders/api.rs
@@ -1,15 +1,64 @@
-use reqwest::Error;
-use serde::Deserialize;
+use super::types;
+use reqwest::{Error, RequestBuilder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Debug)]
+struct RegisterRequest {
+    pub faction: String,
+    pub symbol: String,
+    pub email: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct RegisterResponse {
+    pub data: Registration,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Registration {
+    pub agent: types::Agent,
+    pub contract: types::Contract,
+    pub faction: types::Faction,
+    pub ship: types::Ship,
+    pub token: String,
+}
+
+pub async fn register_new_agent(
+    symbol: String,
+    faction: String,
+    email: String,
+) -> Result<Registration, Error> {
+    let client = reqwest::Client::new();
+    let request_url = "https://api.spacetraders.io/v2/register";
+    let register_request = RegisterRequest {
+        faction,
+        symbol,
+        email,
+    };
+
+    let request_body = serde_json::to_string(&register_request).unwrap();
+
+    let response = client
+        .post(request_url)
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .body(request_body)
+        .send()
+        .await?;
+
+    let register_response: RegisterResponse = response.json().await?;
+    Ok(register_response.data)
+}
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct AgentApiResponse {
-    pub data: super::types::Agent,
+    pub data: types::Agent,
 }
 
 // TODO: Make api methods use the same client and token
 // TODO: Ensure the maximum requests per second is respected
-pub async fn get_agent(token: String) -> Result<super::types::Agent, Error> {
+pub async fn my_agent_details(token: String) -> Result<types::Agent, Error> {
     let client = reqwest::Client::new();
     let request_url = "https://api.spacetraders.io/v2/my/agent";
 
